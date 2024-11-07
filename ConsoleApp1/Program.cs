d782ed5f0fb23e96625ae6b31423a5790e8c41ab
Dulith <dulithramanayake@gmail.com>
2024-11-07 11:00:50 +0530

    update

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Program
+    {
+        static Worker worker = new Worker();
+        static void Main(string[] args)
+        {
+
+
+            //int returnResult = worker.Add(2, 2);
+            //Console.WriteLine(returnResult);
+            //Console.ReadLine();
+
+            CheckIf();
+
+
+        }
+
+        static void CheckIf()
+        {
+            int x = 8;
+
+            if (x > 10)
+            {
+                if (x > 20)
+                {
+                    Console.WriteLine("X is Less than 5");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("X is Less than 5");
+                    Console.ReadLine();
+                }
+
+                //Console.WriteLine("X is Greater than 10");
+                //Console.ReadLine() ;
+            }
+            else if (x < 5)
+            {
+                Console.WriteLine("X is Less than 5");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("X is Less than 10");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp1/Worker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class Worker
+    {
+
+        public void Print()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+        public int Add(int a, int b)
+        {
+            int resut = 0;
+            try 
+            {
+                resut = a + b;
+                return resut;
+            }
+            catch (Exception ex) 
+            {
+
+            }
+            return resut;
+
+
+            
+        }
+    }
+}
